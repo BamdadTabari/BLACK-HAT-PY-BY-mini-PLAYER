4a88cc23c217b1908d79da2f23d223804461d069
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2024-07-13 16:22:35 +0330

    ajaab

--- 0-socket/1-udp.py
+++ 0-socket/1-udp.py
@@ -1,6 +1,6 @@
 import socket
-target_host = "127.0.0.1"
-target_port = 9999
+target_host = "www.google.com"
+target_port = 80
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # send some data
@@ -9,3 +9,25 @@ client.sendto(b"AAABBBCCC",(target_host,target_port))
 data, addr = client.recvfrom(4096)
 print(data.decode())
 client.close()
+
+# import socket
+
+# # Create a UDP socket
+# server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# # Bind the socket to a specific address and port
+# server_address = ('127.0.0.1', 9999)  # Listen on all available interfaces
+# server_socket.bind(server_address)
+
+# while True:
+#     print('\nWaiting to receive message')
+#     data, address = server_socket.recvfrom(4096)
+
+#     print('Received %s bytes from %s' % (len(data), address))
+#     print(data)
+
+#     if data:
+#         sent = server_socket.sendto(b'Received', address)
+#         print('Sent %s bytes back to %s' % (sent, address))
+
+#         server_socket.close()
